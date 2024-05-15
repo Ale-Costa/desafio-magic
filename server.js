1ada6a16c1ff5e79d12df52a81f8fcb98c92da90
Ale Costa <alexandrercosta01@gmail.com>
2024-05-15 18:33:56 -0300

    refac: heroku configs

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
 app.use(express.static(__dirname + '/dist/desafio-linked'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/desafio-linked/index.html'));});
+
+res.sendFile(path.join(__dirname+'/dist/desafio-linked/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
