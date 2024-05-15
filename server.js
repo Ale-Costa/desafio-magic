ada48bb0c8312520d4bb4ff8d31eab0caf7ba7a8
Ale Costa <alexandrercosta01@gmail.com>
2024-05-15 18:41:40 -0300

    feat: ajustando path express

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/desafio-linked'));
+app.use(express.static(__dirname + '/dist/desafio-linked/browser'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/desafio-linked/index.html'));
+res.sendFile(path.join(__dirname+'/dist/desafio-linked/browser/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
